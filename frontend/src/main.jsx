feea06faa1ac083a1499c0366107c60277a8577b
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-18 18:11:57 -0500

    updated some imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
